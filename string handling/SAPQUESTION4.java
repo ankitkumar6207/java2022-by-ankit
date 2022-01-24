ceb7f52a06e5309aec1179650ea0125edaf56909
Ankit kumar <ankitsoni7544@gmail.com>
2022-01-24 13:31:38 +0530

    removed all files

--- string handling/SAPQUESTION4.java
+++ string handling/SAPQUESTION4.java
@@ -3,30 +3,11 @@ import java.util.Scanner;
 public class SAPQUESTION4 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter a string");
-        String str=sc.next();
-        String str2="";
-        
-        int c=0;
-        for(int i=0;i<str.length();i++)
-        {
-            for(int j=i+1;j<str.length();j++)
-            {
-                if(str.charAt(i)==str.charAt(j))
-                {
-                  c++;
-                  break;
-                }
-               
-
-
-            }
-            if(c==0)
-            System.out.print(str.charAt(i));
-            c=0;
-            
-         
-        }
+        // System.out.println("enter a string");
+        // String str=sc.next();
+        String str2="ankit";
+        int in=str2.indexOf('u');
+        System.out.println(in);
 
        
 
